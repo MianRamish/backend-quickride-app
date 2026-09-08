e1217fde498e487ee30f2144b087dd4ed59bc246
Ramish Qamar <95705163+MianRamish@users.noreply.github.com>
2026-09-09 01:03:03 +0500

    Update mail.routes.js

--- routes/mail.routes.js
+++ routes/mail.routes.js
@@ -1,19 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const mailController = require("../controllers/mail.controller");
-<<<<<<< HEAD
 
 // Email is optional and currently used only for password-reset delivery if SMTP is configured.
 router.post("/:userType/reset-password", mailController.forgotPassword);
-=======
-const { body } = require("express-validator");
-const { authUser, authCaptain } = require("../middlewares/auth.middleware");
-
-router.get("/verify-user-email", authUser, mailController.sendVerificationEmail);
-router.get("/verify-captain-email", authCaptain, mailController.sendVerificationEmail);
-
-router.post("/:userType/reset-password",  mailController.forgotPassword);
-
->>>>>>> addc804220915c5314abc19e357f9f2912d7afbc
 
 module.exports = router;
