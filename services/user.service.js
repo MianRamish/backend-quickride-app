72411896c5b775ab2c37055ba0486aeb314c41ae
Ramish Qamar <95705163+MianRamish@users.noreply.github.com>
2026-09-09 01:05:49 +0500

    Update user.service.js

--- services/user.service.js
+++ services/user.service.js
@@ -1,8 +1,5 @@
 const userModel = require("../models/user.model");
-<<<<<<< HEAD
 const { normalizeNigeriaPhone } = require("../utils/nigeria");
-=======
->>>>>>> addc804220915c5314abc19e357f9f2912d7afbc
 
 module.exports.createUser = async (firstname, lastname, email, password, phone) => {
   if (!firstname || !email || !password || !phone) {
@@ -11,11 +8,8 @@ module.exports.createUser = async (firstname, lastname, email, password, phone) 
 
   const hashedPassword = await userModel.hashPassword(password);
 
-<<<<<<< HEAD
   const referralBase = `${String(firstname).replace(/[^a-z0-9]/gi, "").slice(0, 5)}${Math.random().toString(36).slice(2, 7)}`.toUpperCase();
 
-=======
->>>>>>> addc804220915c5314abc19e357f9f2912d7afbc
   const user = await userModel.create({
     fullname: {
       firstname,
@@ -23,12 +17,8 @@ module.exports.createUser = async (firstname, lastname, email, password, phone) 
     },
     email,
     password: hashedPassword,
-<<<<<<< HEAD
     phone: normalizeNigeriaPhone(phone),
     referralCode: referralBase,
-=======
-    phone,
->>>>>>> addc804220915c5314abc19e357f9f2912d7afbc
   });
 
   return user;
